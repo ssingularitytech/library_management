2cf573fb4d0c5a712be1760c26190e4ae292518d
Rishabh Singh <ris8645208@gmail.com>
2024-01-29 11:11:52 +0530

    feat: add slim select

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,23 @@ import "./controllers";
 import "./dropdown";
 import 'preline/dist/preline';
 import 'fslightbox';
+import SlimSelect from 'slim-select'
 
 document.addEventListener("turbo:load", (e) => {
   HSStaticMethods.autoInit();
+
+  const selectTags = document.querySelectorAll('select');
+
+  selectTags.forEach((selectTag) => {
+    new SlimSelect({
+      select: selectTag,
+      showSearch: false,
+      allowDeselect: true,
+      placeholder: true,
+      settings: {
+        closeOnSelect: false,
+        placeholderText: selectTag.dataset.placeholder,
+      }
+    });
+  });
 });
